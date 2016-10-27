9e4d14c751fa65c1c2595c7e259390a891918fa8
Emmanuel Mendoza Acevedo <emmanuel.ma@icloud.com>
2016-10-27 15:42:50 -0500

    Add config manager profile functionality for changing admin options like the passwors.

---
+++ include/language/managerprofile_es_MX.php
@@ -0,0 +1,11 @@
+<?php
+//En for mangaer
+$change_password = "Cambiar Contraseña";
+$current_password = "Contraseña Actual";
+$new_password = "Nueva Contraseña";
+$confirm_new_password = "Confirmar Nueva Contraseña";
+$current_password_not_valid = "Su contraseña actual no es válida";
+$new_password_confirmation_not_valid = "La confirmación de la nueva contraseña no es válida";
+$new_password_saved = "Nuevo password almacenado";
+$change = "Cambiar";
+?>
